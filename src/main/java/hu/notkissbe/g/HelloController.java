91eefe9ffc5a23e39eea3e5897b91c026ad2d1eb
notkissbe <bencetel1@gmail.com>
2025-05-26 00:29:27 +0200

    komit

--- src/main/java/hu/notkissbe/g/HelloController.java
+++ src/main/java/hu/notkissbe/g/HelloController.java
@@ -1,14 +1,36 @@
 package hu.notkissbe.g;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    Media media = new Media(new File("seatbelt.mp3").toURI().toString());
+    MediaPlayer mp = new MediaPlayer(media);
+
+    boolean isClicked = false;
+
+
+
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onGButtonClick() {
+        if(isClicked){
+            mp.stop();
+            isClicked = false;
+        }
+        else{
+            mp.play();
+            isClicked = true;
+        }
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle("Információ!");
+        a.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module hu.notkissbe.g {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens hu.notkissbe.g to javafx.fxml;
